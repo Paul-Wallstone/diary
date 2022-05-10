5ca5dd237fd3ab50c7dbd56971643501e71ebde1
alakhmitski <alakhmitski@elinext.com>
2022-05-11 00:49:57 +0300

    master commit - create project

---
+++ src/test/java/by/school/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.school.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
