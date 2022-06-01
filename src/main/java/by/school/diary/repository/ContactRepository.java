9daba00b65c391929b304d70731a55c1d040739d
alakhmitski <alakhmitski@elinext.com>
2022-06-01 21:42:40 +0300

    dev commit - refactored ContactRepository

--- src/main/java/by/school/diary/repository/ContactRepository.java
+++ src/main/java/by/school/diary/repository/ContactRepository.java
@@ -2,12 +2,10 @@ package by.school.diary.repository;
 
 import by.school.diary.entity.ContactEntity;
 
-import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends CrudRepository<ContactEntity, Long> {
-	Optional<ContactEntity> findById(Long id);
 }
