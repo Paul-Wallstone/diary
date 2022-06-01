40315d22defc63a264124829e93a9b7b8e1b4372
alakhmitski <alakhmitski@elinext.com>
2022-06-01 21:39:15 +0300

    dev commit - refactored ContactRepository

--- src/main/java/by/school/diary/repository/ContactRepository.java
+++ src/main/java/by/school/diary/repository/ContactRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends CrudRepository<ContactEntity, Long> {
-	Optional<ContactEntity> findByContactEntity(ContactEntity contactEntity);
+	Optional<ContactEntity> findById(Long id);
 }
--- src/main/java/by/school/diary/service/impl/ContactServiceImpl.java
+++ src/main/java/by/school/diary/service/impl/ContactServiceImpl.java
@@ -16,52 +16,52 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ContactServiceImpl implements ContactService {
 
-	@Autowired
-	private ContactRepository contactRepository;
-	@Autowired
-	private CustomModelMapper modelMapper;
+    @Autowired
+    private ContactRepository contactRepository;
+    @Autowired
+    private CustomModelMapper modelMapper;
 
-	@Override
-	public ContactResponseDto getById(Long id) {
-		ContactEntity contactEntity = contactRepository.findById(id).orElseThrow(() -> new ContactNotFoundException(id));
-		return modelMapper.toDto(contactEntity);
-	}
+    @Override
+    public ContactResponseDto getById(Long id) {
+        ContactEntity contactEntity = contactRepository.findById(id).orElseThrow(() -> new ContactNotFoundException(id));
+        return modelMapper.toDto(contactEntity);
+    }
 
-	@Override
-	public ContactResponseDto save(ContactRequestDto contactDto) {
-		ContactEntity contactEntity = modelMapper.toEntity(contactDto);
-		ContactEntity savedContactEntity = contactRepository.save(contactEntity);
-		return modelMapper.toDto(savedContactEntity);
-	}
+    @Override
+    public ContactResponseDto save(ContactRequestDto contactDto) {
+        ContactEntity contactEntity = modelMapper.toEntity(contactDto);
+        ContactEntity savedContactEntity = contactRepository.save(contactEntity);
+        return modelMapper.toDto(savedContactEntity);
+    }
 
-	@Override
-	public ContactResponseDto update(ContactRequestDto contactDto, Long id) {
-		ContactEntity contactEntity = contactRepository.findById(id).orElseThrow(() -> new ContactNotFoundException(id));
-		contactEntity.setAddress(contactDto.getAddress());
-		contactEntity.setCity(contactDto.getCity());
-		contactEntity.setPostcode(contactDto.getPostcode());
-		contactEntity.setPhone(contactDto.getPhone());
-		ContactEntity updatedContactEntity = contactRepository.save(contactEntity);
-		return modelMapper.toDto(updatedContactEntity);
-	}
+    @Override
+    public ContactResponseDto update(ContactRequestDto contactDto, Long id) {
+        ContactEntity contactEntity = contactRepository.findById(id).orElseThrow(() -> new ContactNotFoundException(id));
+        contactEntity.setAddress(contactDto.getAddress());
+        contactEntity.setCity(contactDto.getCity());
+        contactEntity.setPostcode(contactDto.getPostcode());
+        contactEntity.setPhone(contactDto.getPhone());
+        ContactEntity updatedContactEntity = contactRepository.save(contactEntity);
+        return modelMapper.toDto(updatedContactEntity);
+    }
 
-	@Override
-	public void delete(ContactRequestDto contactDto) {
-		ContactEntity contactEntity = modelMapper.toEntity(contactDto);
-		if (contactRepository.findByContactEntity(contactEntity).isPresent()) {
-			contactRepository.delete(contactEntity);
-		} else {
-			throw new ContactNotFoundException(contactEntity.getId());
-		}
-	}
+    @Override
+    public void delete(ContactRequestDto contactDto) {
+        ContactEntity contactEntity = modelMapper.toEntity(contactDto);
+        if (contactRepository.findById(contactEntity.getId()).isPresent()) {
+            contactRepository.delete(contactEntity);
+        } else {
+            throw new ContactNotFoundException(contactEntity.getId());
+        }
+    }
 
-	@Override
-	public void deleteById(Long id) {
-		if (contactRepository.existsById(id)) {
-			contactRepository.deleteById(id);
-		} else {
-			throw new ContactNotFoundException(id);
-		}
-	}
+    @Override
+    public void deleteById(Long id) {
+        if (contactRepository.existsById(id)) {
+            contactRepository.deleteById(id);
+        } else {
+            throw new ContactNotFoundException(id);
+        }
+    }
 
 }
